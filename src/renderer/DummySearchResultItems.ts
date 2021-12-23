12102d2f783413d2744206ff8f0ee8a2898f55e1
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-12-23 09:49:55 +0100

    Simplified code

---
+++ src/renderer/DummySearchResultItems.ts
@@ -0,0 +1,28 @@
+import { SearchResultItem } from "./SearchResultItem";
+
+export const dummySearchResultItems: SearchResultItem[] = [
+    {
+        title: "Adobe Lightroom Classic 2021",
+        description: "/Applications/Adobe Lightroom Classic 2021.app",
+        imageUrl: "https://img.icons8.com/color/2x/adobe-lightroom--v2.png",
+        selected: false,
+    },
+    {
+        title: "Adobe Photoshop CC 2021",
+        description: "/Applications/Adobe Photoshop CC 2021.app",
+        imageUrl: "https://img.icons8.com/color/2x/adobe-photoshop--v2.png",
+        selected: false,
+    },
+    {
+        title: "Adobe After Effects CC 2021",
+        description: "/Applications/Adobe After Effects CC 2021.app",
+        imageUrl: "https://img.icons8.com/color/2x/adobe-after-effects--v2.png",
+        selected: false,
+    },
+    {
+        title: "Adobe Premiere Pro CC 2021",
+        description: "/Applications/Adobe Premiere Pro CC 2021.app",
+        imageUrl: "https://img.icons8.com/color/2x/adobe-premiere-pro--v2.png",
+        selected: false,
+    },
+];
--- src/renderer/MainApp.tsx
+++ src/renderer/MainApp.tsx
@@ -1,37 +1,11 @@
 import { Stack, StackItem, TextField } from "@fluentui/react";
 import { ThemeProvider } from "@fluentui/react/lib/Theme";
 import React, { FC, useState } from "react";
+import { dummySearchResultItems } from "./DummySearchResultItems";
 import { SearchResultItem } from "./SearchResultItem";
 import { SearchResultList } from "./SearchResultList";
 import { UeliColorThemes } from "./Themes";
 import { UeliSpacing } from "./UeliSpacing";
-
-const all: SearchResultItem[] = [
-    {
-        title: "Adobe Lightroom Classic 2021",
-        description: "/Applications/Adobe Lightroom Classic 2021.app",
-        imageUrl: "https://img.icons8.com/color/2x/adobe-lightroom--v2.png",
-        selected: false,
-    },
-    {
-        title: "Adobe Photoshop CC 2021",
-        description: "/Applications/Adobe Photoshop CC 2021.app",
-        imageUrl: "https://img.icons8.com/color/2x/adobe-photoshop--v2.png",
-        selected: false,
-    },
-    {
-        title: "Adobe After Effects CC 2021",
-        description: "/Applications/Adobe After Effects CC 2021.app",
-        imageUrl: "https://img.icons8.com/color/2x/adobe-after-effects--v2.png",
-        selected: false,
-    },
-    {
-        title: "Adobe Premiere Pro CC 2021",
-        description: "/Applications/Adobe Premiere Pro CC 2021.app",
-        imageUrl: "https://img.icons8.com/color/2x/adobe-premiere-pro--v2.png",
-        selected: false,
-    },
-];
 
 export const MainApp: FC = () => {
     const theme = UeliColorThemes.WindowsDark;
@@ -40,10 +14,12 @@ export const MainApp: FC = () => {
 
     const selectNext = (): void => {
         const currentlySelectedIndex = searchResultItems.findIndex((s) => s.selected);
-        const nextSelectedIndex = currentlySelectedIndex >= searchResultItems.length ? 0 : currentlySelectedIndex + 1;
+        const nextSelectedIndex =
+            currentlySelectedIndex >= searchResultItems.length - 1 ? 0 : currentlySelectedIndex + 1;
         setSearchResultItems(
             searchResultItems.map(
-                (s, index): SearchResultItem => Object.assign({}, s, { selected: nextSelectedIndex === index })
+                (searchResultItem, index): SearchResultItem =>
+                    Object.assign({}, searchResultItem, { selected: nextSelectedIndex === index })
             )
         );
     };
@@ -54,7 +30,8 @@ export const MainApp: FC = () => {
             currentlySelectedIndex === 0 ? searchResultItems.length - 1 : currentlySelectedIndex - 1;
         setSearchResultItems(
             searchResultItems.map(
-                (s, index): SearchResultItem => Object.assign({}, s, { selected: nextSelectedIndex === index })
+                (searchResultItem, index): SearchResultItem =>
+                    Object.assign({}, searchResultItem, { selected: nextSelectedIndex === index })
             )
         );
     };
@@ -63,15 +40,16 @@ export const MainApp: FC = () => {
         const nextSearchTerm = event.currentTarget.value;
         setSearchTerm(nextSearchTerm);
         setSearchResultItems(
-            all
+            dummySearchResultItems
                 .filter(
-                    (s): boolean =>
-                        nextSearchTerm !== "" && s.title.toLowerCase().indexOf(nextSearchTerm.toLowerCase()) > -1
+                    (searchResultItem): boolean =>
+                        nextSearchTerm !== "" &&
+                        searchResultItem.title.toLowerCase().indexOf(nextSearchTerm.toLowerCase()) > -1
                 )
-                .map((s, index): SearchResultItem => {
-                    s.selected = index === 0;
-                    return s;
-                })
+                .map(
+                    (searchResultItem, index): SearchResultItem =>
+                        Object.assign({}, searchResultItem, { selected: index === 0 })
+                )
         );
     };
 
